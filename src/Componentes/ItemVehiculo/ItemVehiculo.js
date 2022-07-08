47619eedabc1b7b9bffb1ad8170572553e2571b2
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2022-07-08 03:14:27 -0300

    Maquetado de la pagina Modelos. Pagina Modelos-v0.1

---
+++ src/Componentes/ItemVehiculo/ItemVehiculo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImagenVehiculoDefault from '../../Imagenes/VehiculoDefault.png'
+import './ItemVehiculo.css'
+
+export default function ItemVehiculo({id='001',name='Vehiculo 001',price='00.000.000', year='2022', photo=ImagenVehiculoDefault}) {
+    return (
+        <div id={"mod"+id} className='contenedor-ItemVehiculo'>
+            <h3 className='texto-titulo-ItemVehiculo'> {name} </h3>
+            <p className='texto-informacion-ItemVehiculo'> {year} | ${price}</p>
+            <div className='contenedor-imagen-ItemVehiculo'>
+                <img src={photo} alt="Foto"  className='imagen-ItemVehiculo' />
+            </div>                    
+        </div>
+    )
+}
--- src/Paginas/Modelos/Modelos.js
+++ src/Paginas/Modelos/Modelos.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import ItemVehiculo from '../../Componentes/ItemVehiculo/ItemVehiculo';
+import { useModelos } from '../../Hooks/useModelos';
 import './Modelos.css'
 
 export default function Modelos() {
+    const {MODELOS}=useModelos()
     return (
         <div>
-            MODELOS
+            <div className='encabezado-Modelos'>
+                <h1>Descubrí todos los modelos </h1>
+                <div className='contenedor-selectores-Modelos'>
+                    <select name="Filtrar" className='selector-Modelos'> 
+                        <option selected value="0"> Filtar por </option>
+                        <optgroup label="Marca">
+                            <option value="1">Sedan</option>
+                            <option value="2">Hatchback</option>
+                            <option value="3">Pickups y Comerciales</option>
+                            <option value="4">SUVs</option>
+                        </optgroup> 
+                    </select>
+                    <select name="Ordenar" className='selector-Modelos'>
+                        <option selected value="0">Ordenar por </option> 
+                        <optgroup label="Año"> 
+                            <option value="1">Acendente</option> 
+                            <option value="2">Desendente</option> 
+                        </optgroup> 
+                        <optgroup label="Precio"> 
+                            <option value="1">Mas Bajo</option> 
+                            <option value="2">Mas Alto</option> 
+                        </optgroup> 
+                    </select>                  
+                </div>    
+            </div>
+
+            <section className='contenedor-items-vehiculos-Modelos'>
+                {MODELOS.map((vehiculo)=>{
+                   return (
+                        <ItemVehiculo 
+
+                            id={vehiculo.id}
+                            name={vehiculo.name } 
+                            year={vehiculo.year} 
+                            price={vehiculo.price} 
+                            photo={vehiculo.photo} 
+                        />                    
+                   )
+                })}
+            </section>
+            <hr className='linea-inferior-Modelos'/>
         </div>
     )
 }
